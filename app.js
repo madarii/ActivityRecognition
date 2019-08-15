357d259b1c873304f33d25b11a050cfe245d857c
rishabh shirke <rishabhshirke@gmail.com>
2019-08-15 23:37:04 +0530

    Watched classification file and send classification to monitor

--- app.js
+++ app.js
@@ -11,19 +11,24 @@ const io = socket(server);
 
 app.use(express.static('public'));
 
+fs.watch(__dirname + '/python/data/classification.txt',(eventName,fileName)=>{
+  fs.readFile(__dirname + '/python/data/classification.txt','utf-8',(err,data)=>{
+    if(err) return console.log('cannot read classification file',err);
+    io.emit('classification', data);
+  });
+});
+
 io.on('connection',(socket)=>{
   console.log('client connected');
 
   socket.on('data',(data)=>{
     var instance = data.data;
-    fs.writeFileSync(__dirname + '/data/rawdata.csv',instance);
-    // console.log(data);
-    // io.emit('data',data);
+    fs.writeFileSync(__dirname + '/python/data/rawdata.csv',instance);
   })
 
   socket.on('disconnect',()=>{
     console.log('client disconnected')
-    fs.truncate(__dirname + '/data/rawdata.csv',0,()=>{
+    fs.truncate(__dirname + '/python/data/rawdata.csv',0,()=>{
       console.log('raw data file truncated');
     })
   });
